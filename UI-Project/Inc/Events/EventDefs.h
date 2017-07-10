6e7b813c2348da4578375a161e244a0ec0789b22
Eddie <eddie_0226@outlook.com>
2017-07-10 05:50:24 -0400

    Refactor
    Refactored Action and Event classes

--- UI-Project/Inc/Events/EventDefs.h
+++ UI-Project/Inc/Events/EventDefs.h
@@ -11,9 +11,9 @@ namespace Pyro
 
 #define BindEventFunction(Function) { std::bind(Function, this, std::placeholders::_1) }
 
-    template <typename Return, typename Arg>
+    template <typename Arg>
     class Action {
-        typedef std::function<Return(Arg)> FunctionType;
+        typedef std::function<void(Arg)> FunctionType;
 
     private:
         /// Action
@@ -52,12 +52,12 @@ namespace Pyro
             return !(*this == rhs);
         }
 
-        Return invoke(Arg arg) {
-            return function(arg);
+        void invoke(Arg arg) {
+            function(arg);
         }
 
-        Return operator()(Arg arg) {
-            return function(arg);
+        void operator()(Arg arg) {
+            function(arg);
         }
 
         void* InvokingObject() const { return invokingObject; }
@@ -66,43 +66,61 @@ namespace Pyro
     };
 
     class IEvent {
-
+        virtual StringHash EventType() const = 0;
+        virtual String EventTypeName() const = 0;
     };
 
     template <typename Arg>
     class Event : public IEvent {
-        typedef Action<void, Arg> Listener;
+        typedef Action<Arg> EventAction;
 
     private:
-        Vector<Listener*> listeners;
+        Vector<EventAction*> actions;
 
     public:
         Event() = default;
 
         ~Event() {
-            for (uint i = 0; uint < listeners.Size(); ++i) {
-                delete listeners[i];
+            Clear();
+        }
+
+        Event(const Event& rhs) {
+            *this = rhs;
+        }
+
+        Event& operator=(const Event& rhs) {
+            if (*this != &rhs) {
+                Clear();
+                for (uint i = 0; i < rhs.actions.Size(); ++i) {
+                    AddAction(rhs.actions[i]);
+                }
             }
 
-            listeners.Clear();
+            return *this;
         }
 
-        void AddListener(const Listener* listener) {
-            if (!Contains(listener))
-                listeners.Add(listener);
+        void AddAction(const EventAction* action) {
+            if (!Contains(action))
+                actions.Add(action);
         }
 
-        void RemoveListener(const Listener* listener) {
-            listeners.Remove(listener);
+        void RemoveAction(const EventAction* action) {
+            listeners.Remove(action);
         }
 
-        bool Contains(const Listener* listener) const {
-            return listeners.Contains(listener);
+        void Clear() {
+            actions.Clear();
         }
 
-        String EventType() const { return Arg::GetTypeStatic(); }
+        bool Contains(const EventAction* action) const {
+            return actions.Contains(action);
+        }
 
-        static String EventTypeStatic() const { return Arg::GetTypeStatic(); }
+        virtual StringHash EventType() const override { return EventTypeStatic(); }
+        virtual String EventTypeName() const override { return EventTypeNameStatic(); }
+
+        static StringHash EventTypeStatic() const { return Arg::GetTypeNameStatic(); }
+        static String EventTypeNameStatic() const { return Arg::GetTypeStatic(); }
     };
 
 #define EventData(typeName) \
