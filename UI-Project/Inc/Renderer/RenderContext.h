b82973730faaaab32d72f1928c4a16c070716fd6
Eddie Goodman <eddie.goodman@gamesim.com>
2017-07-14 13:44:48 -0400

    Started Render Device / Render Context

--- UI-Project/Inc/Renderer/RenderContext.h
+++ UI-Project/Inc/Renderer/RenderContext.h
@@ -1,9 +1,35 @@
 #pragma once
+#include "../Core/Object.h"
+#include "../Containers/Str.h"
+#include "../Containers/HashMap.h"
 
+#include "Common.h"
+#include "BlendState.h"
+#include "DepthStencilState.h"
+#include "RasterizerState.h"
+#include "SamplerState.h"
 
 namespace Pyro
 {
-    class RenderContext {
+	class RenderDevice;
 
-    };
+	class RenderContext : public RefCounted {
+		REFCOUNTED(RenderContext);
+
+		friend class RenderDevice;
+
+	private:
+		HashMap<uint, SharedPtr<BlendState>> blendStates;
+		HashMap<uint, SharedPtr<DepthStencilState>> depthStencilStates;
+		HashMap<uint, SharedPtr<RasterizerState>> rasterizerStates;
+		HashMap<uint, SharedPtr<SamplerState>> samplerStates;
+
+	public:
+		/// Construct
+		RenderContext();
+		/// Destruct
+		~RenderContext();
+
+		
+	};
 }
--- UI-Project/Inc/Renderer/RenderDevice.h
+++ UI-Project/Inc/Renderer/RenderDevice.h
@@ -1,9 +1,45 @@
 #pragma once
+#include "../Core/Object.h"
+#include "../Containers/Str.h"
 
+#include "Common.h"
+#include "BlendState.h"
+#include "DepthStencilState.h"
+#include "RasterizerState.h"
+#include "SamplerState.h"
 
 namespace Pyro
 {
-	class RenderDevice {
+	class RenderContext;
 
+	class RenderDevice : public RefCounted {
+		REFCOUNTED(RenderDevice);
+
+	private:
+		/// D3D Device
+		DevicePtr device;
+		/// Render context
+		RenderContext* renderContext;
+
+	public:
+		/// Construct
+		RenderDevice();
+		/// Destruct
+		~RenderDevice();
+
+		/// Creates the D3D device
+		Result CreateDevice(RenderContext* renderContext);
+
+		/// Create a blend state and add it to the render context if successfully created
+		Result CreateBlendState(const String& name, const BlendStateDescription& desc);
+		/// Create a depth stencil state and add it to the render context if successfully created
+		Result CreateDepthStencilState(const String& name, const DepthStencilState& desc);
+		/// Create a rasterizer state and add it to the render context if successfully created
+		Result CreateRasterizerState(const String& name, const RasterizerStateDescription& desc);
+		/// Create a sampler state and add it to the render context if successfully created
+		Result CreateSamplerState(const String& name, const SamplerState& desc);
+
+		/// Return D3D device
+		DevicePtr GetDevice() const { return device; }
 	};
 }
